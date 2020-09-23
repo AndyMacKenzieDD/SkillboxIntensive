5aed11e9ad4df2c4a78250e2e95464cdec3b8204
Андрей Савицкий <>
2020-09-24 00:11:41 +0300

    Asteroid added

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float minSpeed;
+    public float maxSpeed;
+
+    public GameObject asteroidExplosion;
+    public GameObject playerExplosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody asteroid = GetComponent<Rigidbody>();
+        asteroid.velocity = new Vector3(0, 0, -Random.Range (minSpeed, maxSpeed));
+        asteroid.angularVelocity = Random.insideUnitSphere * rotationSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(asteroidExplosion, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
